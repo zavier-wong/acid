3d90b74c81e458771d778af64dca64115731a7b0
zavier <acid@163.com>
2022-01-15 23:47:05 +0800

    RPC服务注册中心

--- acid/rpc/rpc_service_registry.h
+++ acid/rpc/rpc_service_registry.h
@@ -4,11 +4,24 @@
 
 #ifndef ACID_RPC_SERVICE_REGISTRY_H
 #define ACID_RPC_SERVICE_REGISTRY_H
+#include <string>
 namespace acid::rpc {
 /**
  * @brief RPC服务注册中心
  */
 class RpcServiceRegistry {
+public:
+    /**
+     * 为服务端提供注册
+     * 将服务地址注册到对应服务名下
+     * 断开连接后地址自动清除
+     * @param serviceName 服务名称
+     * @param serviceAddress 服务地址
+     */
+    void registerService(const std::string& serviceName, const std::string& serviceAddress) {
+
+    }
+private:
 
 };
 }
