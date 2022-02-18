8ecc20e3f19d2e55ddf1a811e469936ffc086d54
zavier <acid@163.com>
2022-02-19 00:10:50 +0800

    协议增加序列号字段

--- acid/rpc/protocol.h
+++ acid/rpc/protocol.h
@@ -9,15 +9,16 @@
 namespace acid::rpc {
 /*
  * 私有通信协议
- * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
- * |  BYTE  |        |        |        |        |        |        |             ........                                                           |
- * +--------------------------------------------+--------+-----------------+--------+--------+--------+--------+--------+--------+-----------------+
- * |  magic | version|  type  |          content length           |             content byte[]                                                     |
- * +--------+-----------------------------------------------------------------------------------------+--------------------------------------------+
+ * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+ * |  BYTE  |        |        |        |        |        |        |        |        |        |        |             ........                                                           |
+ * +--------------------------------------------+--------+--------------------------+--------+-----------------+--------+--------+--------+--------+--------+--------+-----------------+
+ * |  magic | version|  type  |          sequence id              |          content length           |             content byte[]                                                     |
+ * +--------+-----------------------------------------------------------------------------------------------------------------------------+--------------------------------------------+
  * 第一个字节是魔法数。
  * 第二个字节代表协议版本号，以便对协议进行扩展，使用不同的协议解析器。
- * 第三个字节是请求类型，如0代表请求1代表响应。
- * 第四个字节表示消息长度，即此四个字节后面此长度的内容是消息content。
+ * 第三个字节是请求类型，如心跳包，rpc请求。
+ * 第四个字节开始是一个32位序列号。
+ * 第七个字节开始的四字节表示消息长度，即后面要接收的内容长度。
  */
 class Protocol {
 public:
@@ -25,7 +26,7 @@ public:
 
     static constexpr uint8_t MAGIC = 0xcc;
     static constexpr uint8_t DEFAULT_VERSION = 0x01;
-    static constexpr uint8_t BASE_LENGTH = 7;
+    static constexpr uint8_t BASE_LENGTH = 11;
 
     enum class MsgType : uint8_t {
         HEARTBEAT_PACKET,       // 心跳包
@@ -45,10 +46,11 @@ public:
         RPC_SERVICE_DISCOVER_RESPONSE
     };
 
-    static Protocol::ptr Create(MsgType type, const std::string& content) {
+    static Protocol::ptr Create(MsgType type, const std::string& content, uint32_t id = 0) {
         Protocol::ptr proto = std::make_shared<Protocol>();
         proto->setMsgType(type);
         proto->setContent(content);
+        proto->setSequenceId(id);
         return proto;
     }
 
@@ -60,12 +62,14 @@ public:
     uint8_t getMagic() const { return m_magic;}
     uint8_t getVersion() const { return m_version;}
     MsgType getMsgType() const { return static_cast<MsgType>(m_type);}
+    uint32_t getSequenceId() const { return m_sequence_id;}
     uint32_t getContentLength() const { return m_content_length;}
     const std::string& getContent() const { return m_content;}
 
     void setMagic(uint8_t magic) { m_magic = magic;}
     void setVersion(uint8_t version) { m_version = version;}
     void setMsgType(MsgType type) { m_type = static_cast<uint8_t>(type);}
+    void setSequenceId(uint32_t id) { m_sequence_id = id;}
     void setContentLength(uint32_t len) { m_content_length = len;}
     void setContent(const std::string& content) { m_content = content;}
 
@@ -74,6 +78,7 @@ public:
         bt->writeFuint8(m_magic);
         bt->writeFuint8(m_version);
         bt->writeFuint8(m_type);
+        bt->writeFuint32(m_sequence_id);
         bt->writeFuint32(m_content.size());
         bt->setPosition(0);
         return bt;
@@ -84,6 +89,7 @@ public:
         bt->writeFuint8(m_magic);
         bt->writeFuint8(m_version);
         bt->writeFuint8(m_type);
+        bt->writeFuint32(m_sequence_id);
         bt->writeStringF32(m_content);
         bt->setPosition(0);
         return bt;
@@ -93,6 +99,7 @@ public:
         m_magic = bt->readFuint8();
         m_version = bt->readFuint8();
         m_type = bt->readFuint8();
+        m_sequence_id = bt->readFuint32();
         m_content_length = bt->readFuint32();
     }
 
@@ -100,6 +107,7 @@ public:
         m_magic = bt->readFuint8();
         m_version = bt->readFuint8();
         m_type = bt->readFuint8();
+        m_sequence_id = bt->readFuint32();
         m_content = bt->readStringF32();
         m_content_length = m_content.size();
     }
@@ -109,6 +117,7 @@ public:
         ss << "[ magic=" << m_magic
             << " version=" << m_version
             << " type=" << m_type
+            << " id=" << m_sequence_id
             << " length=" << m_content_length
             << " content=" << m_content
             << " ]";
@@ -118,6 +127,7 @@ private:
     uint8_t m_magic = MAGIC;
     uint8_t m_version = DEFAULT_VERSION;
     uint8_t m_type = 0;
+    uint32_t m_sequence_id = 0;
     uint32_t m_content_length = 0;
     std::string m_content;
 };
