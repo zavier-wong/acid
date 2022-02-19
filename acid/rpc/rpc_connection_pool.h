0330c5862ed836e22202250029f79c62428b17f8
zavier <acid@163.com>
2022-02-19 16:36:38 +0800

    修复连接池并发崩溃

--- acid/rpc/rpc_connection_pool.h
+++ acid/rpc/rpc_connection_pool.h
@@ -4,7 +4,6 @@
 
 #ifndef ACID_RPC_CONNECTION_POOL_H
 #define ACID_RPC_CONNECTION_POOL_H
-#include "acid/ds/lru_map.h"
 #include "acid/traits.h"
 #include "acid/mutex.h"
 #include "route_strategy.h"
@@ -20,7 +19,7 @@ public:
     using ptr = std::shared_ptr<RpcConnectionPool>;
     using MutexType = Mutex;
 
-    RpcConnectionPool(size_t capacity,uint64_t timeout_ms = -1);
+    RpcConnectionPool(uint64_t timeout_ms = -1);
     ~RpcConnectionPool();
     /**
      * @brief 连接 RPC 服务中心
@@ -29,6 +28,32 @@ public:
     bool connect(Address::ptr address);
 
     void stop();
+    /**
+     * @brief 用户可手动服务发现
+     * @param name 服务名称
+     * @return 通过服务的地址数量
+     */
+    [[maybe_unused]]
+    size_t discover(const std::string& name) {
+        std::vector<std::string> addrs = discover_impl(name);
+        if (addrs.size()) {
+            // 选择客户端负载均衡策略，根据路由策略选择服务地址
+            acid::rpc::RouteStrategy<std::string>::ptr strategy =
+                    acid::rpc::RouteEngine<std::string>::queryStrategy(
+                            acid::rpc::RouteStrategy<std::string>::Random);
+            const std::string& ip = strategy->select(addrs);
+            Address::ptr address = Address::LookupAny(ip);
+            // 选择的服务地址有效
+            if (address) {
+                RpcClient::ptr client = std::make_shared<RpcClient>();
+                // 成功连接上服务器
+                if (client->connect(address)) {
+                    m_conns.template emplace(name, client);
+                }
+            }
+        }
+        return addrs.size();
+    }
 
     /**
      * @brief 异步远程过程调用回调模式
@@ -72,20 +97,23 @@ public:
      */
     template<typename R, typename... Params>
     Result<R> call(const std::string& name, Params... ps) {
+        MutexType::Lock lock(m_connMutex);
         // 从连接池里取出服务连接
-        auto conn = m_conns.get(name);
+        auto conn = m_conns.find(name);
         Result<R> result;
-        if (conn) {
-            result = (*conn)->template call<R>(name, ps...);
+        if (conn != m_conns.end()) {
+            lock.unlock();
+            result = conn->second->template call<R>(name, ps...);
             if (result.getCode() != RPC_CLOSED) {
                 return result;
             }
+            lock.lock();
             // 移除失效连接
-            std::vector<std::string> addrs = m_serviceCache[name];
+            std::vector<std::string>& addrs = m_serviceCache[name];
             addrs.erase(std::remove(addrs.begin(), addrs.end(),
-                                    (*conn)->getSocket()->getRemoteAddress()->toString()));
+                                    conn->second->getSocket()->getRemoteAddress()->toString()));
 
-            m_conns.remove(name);
+            m_conns.erase(name);
         }
 
         std::vector<std::string>& addrs = m_serviceCache[name];
@@ -97,7 +125,7 @@ public:
                 res.setMsg("registry closed");
                 return res;
             }
-            addrs = discover(name);
+            addrs = discover_impl(name);
             // 如果没有发现服务返回错误
             if (addrs.empty()) {
                 Result<R> res;
@@ -112,7 +140,7 @@ public:
                 acid::rpc::RouteEngine<std::string>::queryStrategy(
                         acid::rpc::RouteStrategy<std::string>::Random);
 
-        while (addrs.size()) {
+        if (addrs.size()) {
             const std::string& ip = strategy->select(addrs);
             Address::ptr address = Address::LookupAny(ip);
             // 选择的服务地址有效
@@ -120,8 +148,9 @@ public:
                 RpcClient::ptr client = std::make_shared<RpcClient>();
                 // 成功连接上服务器
                 if (client->connect(address)) {
-                    m_conns.set(name, client);
-                    return call<R>(name, ps...);
+                    m_conns.template emplace(name, client);
+                    lock.unlock();
+                    return client->template call<R>(name, ps...);
                 }
             }
         }
@@ -137,16 +166,17 @@ private:
      * @param name 服务名称
      * @return 服务地址列表
      */
-    std::vector<std::string> discover(const std::string& name);
+    std::vector<std::string> discover_impl(const std::string& name);
 
 private:
     uint64_t m_timeout;
-    MutexType m_mutex;
-    //std::list<RpcClient::ptr> m_conns;
+    MutexType m_sendMutex;
+
+    MutexType m_connMutex;
     /// 服务名到全部缓存的服务地址列表映射
     std::map<std::string, std::vector<std::string>> m_serviceCache;
     /// 服务名和服务地址的连接池
-    LRUMap<std::string, RpcClient::ptr> m_conns;
+    std::map<std::string, RpcClient::ptr> m_conns;
     /// 服务中心连接
     RpcSession::ptr m_registry;
 
