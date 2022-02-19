fdd952e0d32c56bc5f40ab1390990de359fa220d
zavier <acid@163.com>
2022-02-19 16:40:57 +0800

    更新测试用例

--- tests/rpc/test_rpc_connection_poll.cpp
+++ tests/rpc/test_rpc_connection_poll.cpp
@@ -9,10 +9,14 @@ static acid::Logger::ptr g_logger = ACID_LOG_ROOT();
 void test_discover() {
     acid::Address::ptr address = acid::Address::LookupAny("127.0.0.1:8070");
     //acid::rpc::RpcConnectionPool::ptr con = std::make_shared<acid::rpc::RpcConnectionPool>(5);
-    acid::rpc::RpcConnectionPool con{5};
+    acid::rpc::RpcConnectionPool con{};
     con.connect(address);
 
-    auto aa = con.call<int>("add",123,321);
+    auto aa = con.call<int>("add",1,1);
+    ACID_LOG_INFO(g_logger) << aa.toString();
+    aa = con.call<int>("add",2,2);
+    ACID_LOG_INFO(g_logger) << aa.toString();
+    aa = con.call<int>("add",3,3);
     ACID_LOG_INFO(g_logger) << aa.toString();
     //std::future<acid::rpc::Result<std::string>> b = con.async_call<std::string>("getStr");
 
@@ -23,16 +27,17 @@ void test_discover() {
     con.async_call<std::string>([](acid::rpc::Result<std::string> str){
         ACID_LOG_INFO(g_logger) << str.toString();
     }, "CatString",vec);
+    sleep(4);
     int n=0;
-    while (false) {
+    while (n!=10000) {
         ACID_LOG_DEBUG(g_logger) << n++;
-//            client->async_call<int>([](acid::rpc::Result<int> res){
-//                ACID_LOG_DEBUG(g_logger) << res.toString();
-//            },"add",0,n);
-        auto rt = con.call<int>("add",0,n);
-        ACID_LOG_DEBUG(g_logger) << rt.toString();
+            con.async_call<int>([](acid::rpc::Result<int> res){
+                ACID_LOG_DEBUG(g_logger) << res.toString();
+            },"add",0,n);
+//        auto rt = con.call<int>("add",0,n);
+//        ACID_LOG_DEBUG(g_logger) << rt.toString();
     }
-    //sleep(5);
+    sleep(5);
 //    ACID_LOG_INFO(g_logger) << b.get().toString();
 //    ACID_LOG_INFO(g_logger) << a.get().toString();
 }
