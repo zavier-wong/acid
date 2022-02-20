fabbb5c8e7304a222941af47e130169f3e381f78
zavier <acid@163.com>
2022-02-21 07:01:00 +0800

    更新测试用例

--- tests/test_uri.cpp
+++ tests/test_uri.cpp
@@ -9,7 +9,7 @@ static Logger::ptr g_logger = ACID_LOG_ROOT();
 
 
 int main(){
-    std::string http = "www.vvip.icu:8080/blog/";
+    std::string http = "www.baidu.com/s?wd=%E4%BD%A0%E5%A5%BD";
     std::string https = "https://baidu.com/add?a=1&b=2#aaa";
     std::string ftp = "ftp://admin:passwd@www.myftp.com/profile";
     std::string file = "file:///c:/desktop/a.cpp";
