e889114849ac76b8e7b44c734ecaaa42a7383bc2
zavier <acid@163.com>
2022-03-04 01:12:07 +0800

    增加协程同步原语

---
+++ acid/sync.h
@@ -0,0 +1,21 @@
+//
+// Created by zavier on 2022/3/2.
+//
+
+#ifndef ACID_SYNC_H
+#define ACID_SYNC_H
+
+/**
+ * @brief 包含了协程同步原语
+ * CoMutex      协程锁
+ * CoCondvar    协程条件变量
+ * CoSemaphore  协程信号量
+ * Channel      消息通道
+ */
+
+#include "sync/channel.h"
+#include "sync/co_condvar.h"
+#include "sync/co_semaphore.h"
+#include "sync/mutex.h"
+
+#endif //ACID_SYNC_H
