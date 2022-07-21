f4c1470fccc8741188a45dfa3a32d2df3518ae7a
zavier-wong <acid@163.com>
2022-07-21 16:19:29 +0800

    测试defer

--- tests/test_util.cpp
+++ tests/test_util.cpp
@@ -10,8 +10,21 @@ void test( int n){
     //ACID_STATIC_ASSERT(c);
     //acid::Assert(0,"Void test(int)");
 }
+void test_defer() {
+    Defer_ {
+        LOG_DEBUG << "no log";
+    };
+    Defer {
+          LOG_DEBUG << "defer c";
+          LOG_DEBUG << "defer d";
+    };
+    defer []{
+        LOG_DEBUG << "defer a";
+    };
+    defer []{
+        LOG_DEBUG << "defer b";
+    };
+}
 int main(){
-    std::vector<std::string> vec{"aa","bb"};
-
-    std::cout<<vec.size();
+    test_defer();
 }
