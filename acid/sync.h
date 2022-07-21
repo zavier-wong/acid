a24a0f3ad895413491a6c9e37ecab85378aae9cf
zavier-wong <acid@163.com>
2022-07-21 16:23:51 +0800

    增加协程计数器

--- acid/sync.h
+++ acid/sync.h
@@ -9,12 +9,14 @@
  * @brief 包含了协程同步原语
  * CoMutex      协程锁
  * CoCondvar    协程条件变量
+ * CoCountDownLatch 协程计数器
  * CoSemaphore  协程信号量
  * Channel      消息通道
  */
 
 #include "sync/channel.h"
 #include "sync/co_condvar.h"
+#include "sync/co_count_down_latch.h"
 #include "sync/co_semaphore.h"
 #include "sync/mutex.h"
 
