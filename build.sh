88795b1f78b2688b2141284ebffb4d43c0a4ead0
zavier <acid@163.com>
2022-11-21 17:00:08 +0800

    build 脚本

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo pwd
+mkdir -p third_party/libgo/build
+mkdir -p third_party/spdlog/build
+mkdir -p third_party/yaml-cpp/build
+mkdir -p build
+
+cd third_party/libgo/build
+cmake ..
+sudo make install
+cd -
+
+cd third_party/spdlog/build
+cmake ..
+sudo make install
+cd -
+
+cd third_party/yaml-cpp/build
+cmake ..
+sudo make install
+cd -
+
+cd build
+cmake ..
+make
+cd ..
